b8f2143bee1fe429cd114fa90f4e2de34b0e952d
TOPRPA <a09730230@gmail.com>
2022-05-08 22:58:45 +0800

    2nd test

--- pass_continue_pass.py
+++ pass_continue_pass.py
@@ -1,7 +1,7 @@
 count=0
 for string in 'content':
     if string == 't':
-        break  #強制跳出整個迴圈
-        #continue #跳過本次，繼續下個迴圈
+        #break  #強制跳出整個迴圈
+        continue #跳過本次，繼續下個迴圈
         #pass #什麼事都不做
     print (string)
